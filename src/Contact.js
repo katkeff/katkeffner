68e3938182df6d269eb731722913984c01ec21dc
Kat Keff <katkeffner@gmail.com>
2022-12-01 16:10:29 -0500

    spotify icon and href added

--- src/Contact.js
+++ src/Contact.js
@@ -1,6 +1,6 @@
 import React from "react";
 import './styles/contact.css'
-import { Instagram, Linkedin, Github, EnvelopePaperHeart } from 'react-bootstrap-icons';
+import { Instagram, Linkedin, Github, EnvelopePaperHeart, Spotify } from 'react-bootstrap-icons';
 
 function Contact() {
   return (
@@ -17,6 +17,9 @@ function Contact() {
         </a>
         <a href={"mailto: kat.keffner@gmail.com"}>
           <EnvelopePaperHeart className="mail" />
+        </a>
+        <a href={"https://open.spotify.com/artist/38fY1KAyeqBN8xascAAJQ6?si=eHSX4TLHTyaxplG1aNScEg&nd=1"}>
+          <Spotify className="spotify" />
         </a>
     </div>
   )
