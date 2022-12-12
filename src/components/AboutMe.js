32aa739ab433744c1776b36c3d771f78a07e9906
Kat Keff <katkeffner@gmail.com>
2022-12-12 16:07:36 -0500

    fixed about me image

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -8,18 +8,17 @@ function AboutMe() {
   const [over, setOver] = useState(false);
   return (
     <div className="aboutcontainer">  
-    <div className='aboutmetxt'>
-      <h2>Hi, I'm Kat!</h2>
-      <p>I'm a Full Stack Developer from New York.</p>
-      <p>I have a passion for innovation and creativity.</p>
-      <p>I utilized my skills as a self-taught musician and graphic designer to teach myself how to code.</p>
-      <p>To further grow as a developer, I attended a coding bootcamp: 
-          <a href="https://www.gracehopper.com/"> The Grace Hopper Program.</a>
-      </p>
-      <p>Now, my ambition as a developer is to be a part of something meaningful, and bigger than myself.</p>
-      <p>Please check out my portfolio below, and let's get connected!</p>
-    </div>
-    <div className="KatLogo"
+      <div className='aboutmetxt'>
+        <h2>Hi, I'm Kat!</h2>
+        <p>I'm a Full Stack Developer from New York.</p>
+        <p>I have a passion for innovation and creativity.</p>
+        <p>I utilized my skills as a self-taught musician and graphic designer to teach myself how to code.</p>
+        <p>To further grow as a developer, I attended a coding bootcamp: 
+            <a href="https://www.gracehopper.com/"> The Grace Hopper Program.</a>
+        </p>
+        <p>Now, my ambition as a developer is to be a part of something meaningful, and bigger than myself.</p>
+        <p>Please check out my portfolio below, and let's get connected!</p>
+      <div className="KatLogo"
       onMouseOver={() => setOver(true)}
       onMouseOut={() => setOver(false)}
       >
@@ -27,7 +26,8 @@ function AboutMe() {
           src={over ? katCool : kat}
           alt={'insert cool pic of kat here'}
         />
-    </div>
+      </div>
+      </div>
     </div>
   );
 }
