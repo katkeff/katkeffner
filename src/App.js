59e97bca586489757e135abda60c6506ca99dd1a
Kat Keff <katkeffner@gmail.com>
2022-12-13 17:09:50 -0500

    theme switch button added

--- src/App.js
+++ src/App.js
@@ -2,11 +2,14 @@ import AboutMe from './components/AboutMe';
 import Projects from './components/Projects';
 import Contact from './components/Contact';
 import Skills from './components/Skills';
-import './styles/app.css';
+import DarkModeBtn from './components/DarkModeBtn';
+import './styles/links.css';
+import './styles/darkMode.css'
 
 function App() {
   return (
     <div className="App">
+      <DarkModeBtn />
       <AboutMe />
       <Skills />
       <Projects />
---
+++ src/components/DarkModeBtn.js
@@ -0,0 +1,21 @@
+import React, { useState, useEffect } from 'react';
+import '../styles/darkMode.css'
+function DarkModeBtn() {
+  const [theme, setTheme] = useState('light');
+  const toggleTheme = () => {
+    if (theme === 'light') {
+    setTheme('dark');
+    } else {
+    setTheme('light');
+    }
+  };
+useEffect(() => {
+  document.body.className = theme;
+  }, [theme]);
+  return (
+  <div className={`App ${theme}`}>
+    <button className='button-78'onClick={toggleTheme}>toggle theme</button>
+  </div>
+  );
+}
+export default DarkModeBtn;
