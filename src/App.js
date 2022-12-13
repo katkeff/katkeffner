d63836cd55c68313c44dd798be91ad94c7142155
Kat Keff <katkeffner@gmail.com>
2022-12-12 21:08:23 -0500

    custom background added

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import AboutMe from './components/AboutMe';
 import Projects from './components/Projects';
 import Contact from './components/Contact';
 import Skills from './components/Skills';
-
 import './styles/app.css';
 
 function App() {
