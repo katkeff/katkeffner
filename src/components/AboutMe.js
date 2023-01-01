2bf570761815a23ffe15492ba74edca7cdbed240
Kat Keff <katkeffner@gmail.com>
2023-01-01 18:20:38 -0500

    spacing issue fixed

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -17,7 +17,7 @@ function AboutMe() {
         <p>I utilized my skills as a self-taught musician and graphic designer to teach myself how to code.</p>
         <p>To further grow as a developer, I attended a coding bootcamp: 
             <a href="https://www.gracehopper.com/"> The Grace Hopper Program.</a>
-        and was awarded a scholarship through 
+        and was awarded a scholarship through&nbsp;
             <a href= "https://lesbianswhotech.org/pridesummit2023/">
               Lesbians Who Tech.</a>
         </p>
